00776dffe5bdeedd684d68ccd39f117a8a9bf713
Sashka <aitmn08@gmail.com>
2023-05-03 23:30:33 +0500

    feat: add css selectors for choseClientModal

--- elements/choseClientModal.js
+++ elements/choseClientModal.js
@@ -2,4 +2,20 @@ const { I } = inject();
 
 module.exports = {
   modalWindow: "body > div.MuiDialog-root > div.MuiDialog-container > div",
+  elements: {
+    choseClientInput: ".MuiAutocomplete-root input",
+    createClientButton: ".MuiDialog-scrollPaper .MuiButton-root",
+    closeModalButton: ".MuiBox-root .MuiSvgIcon-root > svg",
+    clearClientInput: ".MuiInput-root .MuiAutocomplete-endAdornment",
+    submitButton:
+      ".MuiDialog-scrollPaper > div > div > div > header > div > button",
+  },
+  clickSubmitButton() {
+    I.click(this.elements.submitButton);
+    I.see("Добавить клиента");
+  },
+  choseClientFromList(){
+    I.click(this.elements.choseClientInput)
+  }
+
 };
