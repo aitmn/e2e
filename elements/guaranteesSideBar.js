75b87ba7417871f0cd3c9c7a8c90230f5b99831c
Aleksandr Ivanov <ivanov@finleo.ru>
2023-05-03 16:15:43 +0500

    fix: naming for properties

--- elements/guaranteesSideBar.js
+++ elements/guaranteesSideBar.js
@@ -6,21 +6,21 @@ module.exports = {
     "body > .MuiDrawer-modal > .MuiDrawer-paper > .ScrollbarsCustom > div > div > span",
   search: "body > div > div > div > div > div > div > div > input",
   guaranteeType: {
-    bg: {
+    bankovskieGarantii: {
       xpath:
         ".//p[ancestor::li[ancestor::ul][contains(., 'Банковские гарантии')]]",
     },
   },
   guarantees: {
-    bgExecution: {
+    bgNaIspolnenie: {
       xpath:
         ".//p[ancestor::li[ancestor::ul][contains(., 'БГ на исполнение')]]",
     },
   },
 
   choseGuarantee() {
-    I.moveCursorTo(this.guaranteeType.bg);
+    I.moveCursorTo(this.guaranteeType.bankovskieGarantii);
     I.seeElement(this.guaranteesSideBar);
-    I.click(this.guarantees.bgExecution);
+    I.click(this.guarantees.bgNaIspolnenie);
   },
 };
