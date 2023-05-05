c6d5fa73eac504221e122b997bd6a25b7ee47904
Aleksandr Ivanov <ivanov@finleo.ru>
2023-05-05 14:04:52 +0500

    feat: create addClientModal

---
+++ elements/addClientModal.js
@@ -0,0 +1,20 @@
+const { I } = inject();
+
+module.exports = {
+  modalWindow: ".MuiDialog-paper > div",
+  fields: {
+    country: "#mui-component-select-country",
+    inn: "form input[name='inn']",
+    name: "form input[name='name']",
+  },
+  buttons: {
+    submit: ".MuiButtonBase-root[type=submit] > span",
+    back: "header > div > button > span",
+    close: "header > div > svg",
+  },
+  radio: {
+    legal: ".MuiFormGroup-root input[value='2']",
+    individualPerson: ".MuiFormGroup-root input[value='1']",
+    individual: ".MuiFormGroup-root input[value='3']",
+  },
+};
