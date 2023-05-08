28d656200a3ffbabeb10cc8a50d131f878a1f079
Sashka <aitmn08@gmail.com>
2023-05-08 11:50:48 +0500

    feat: add test for create client

--- tests/createClient_test.js
+++ tests/createClient_test.js
@@ -1,7 +1,14 @@
 const { radio } = require("../elements/addClientModal");
 
-Feature("Создание клиента")
-Before(({ I, signInPage, appBarElement, GuaranteesSideBarElement, choseClientModalElement }) => {
+Feature("Создание клиента");
+Before(
+  ({
+    I,
+    signInPage,
+    appBarElement,
+    GuaranteesSideBarElement,
+    choseClientModalElement,
+  }) => {
     I.amOnPage(process.env.BASE_URL);
     signInPage.signIn(process.env.AGENT_EMAIL, process.env.BASE_PASSWORD);
     I.wait(3);
@@ -9,5 +16,10 @@ Before(({ I, signInPage, appBarElement, GuaranteesSideBarElement, choseClientMod
     I.wait(2);
     GuaranteesSideBarElement.choseGuarantee();
     choseClientModalElement.clickCreateClientButton();
-  })
+  }
+);
 
+Scenario.only("Нельзя выбрать Физическое лицо", async ({ I }) => {
+  let isDisabled = await I.grabAttributeFrom(radio.individual, "aria-disabled");
+  if (isDisabled === "true") console.log("Нельзя выбрать Физ. лицо");
+});
