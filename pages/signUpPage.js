af9ef1df3dbaa7ebb2ad69afa07f3b8af18728bb
Aleksandr Ivanov <ivanov@finleo.ru>
2023-05-29 18:56:42 +0500

    fix: edit methods for signUp page

--- pages/signUpPage.js
+++ pages/signUpPage.js
@@ -15,7 +15,7 @@ module.exports = {
       passwordConfirm: "input[name='passwordConfirm']",
       inn: "input[name='inn']",
       nonResidentInn: "input[name='nonResidentInn']",
-      nonResidentCompanyName: "input[name='nonResidentName']"
+      nonResidentCompanyName: "input[name='nonResidentName']",
     },
     buttons: {
       back: ".MuiButtonBase-root:nth-child(1)",
@@ -28,13 +28,13 @@ module.exports = {
     listElements: {
       userClasses: {
         legal: "li[data-value='LEGAL']",
-        individual: "li[data-value='INDIVIDUAL']"
+        individual: "li[data-value='INDIVIDUAL']",
       },
       userRoles: {
         agent: ".//li[contains(., 'Агент')]",
         client: ".//li[contains(., 'Клиент')]",
         diler: ".//li[contains(., 'Дилер')]",
-        partner: ".//li[contains(., 'Партнер')]"
+        partner: ".//li[contains(., 'Партнер')]",
       },
       countries: {
         Armenia: "li[data-value='AM']",
@@ -45,41 +45,72 @@ module.exports = {
         Moldavia: "li[data-value='MD']",
         Russia: "li[data-value='RU']",
         Tajikistan: "li[data-value='TJ']",
-        Uzbekistan: "li[data-value='UZ']"
-      }
+        Uzbekistan: "li[data-value='UZ']",
+      },
     },
-      lists: "ul.MuiList-root"
+    lists: "ul.MuiList-root",
   },
-  clickSubmitButton(){
-    I.click(this.buttons.submit)
+  credentials: {
+    password: "12345",
+    Moldavia: {
+      inn: "1010600022460",
+      companyName: "SRL 'MOLDRETAIL GROUP'",
+      phone: "22737794",
+      email: "info@infobiz.md",
+    },
   },
-  clickConfirmTerms(){
-    I.click(this.checkboxes.confirmTerms)
+  clickSubmitButton() {
+    I.click(this.buttons.submit);
   },
-  createLegalAgentMoldova(){
-    I.click(this.elements.selects.userClass)
-    I.seeElement(this.elements.lists)
-    I.wait(1)
-    I.click(this.elements.listElements.userClasses.legal)
-    I.click(this.elements.selects.userRole)
-    I.seeElement(this.elements.lists)
-    I.click(this.elements.listElements.userRoles.agent)
-    I.click(this.elements.selects.country)
-    I.seeElement(this.elements.lists)
-    I.click(this.elements.listElements.Moldavia)
-    I.dontSeeElement(this.elements.fields.inn)
-    I.seeElement(this.elements.fields.nonResidentInn)
-    I.seeElement(this.elements.fields.nonResidentCompanyName)
-    I.fillFIeld(this.elements.fields.nonResidentInn, "1010600022460")
-    I.fillfiled(this.elements.fields.nonResidentCompanyName, "SRL 'MOLDRETAIL GROUP'")
-    I.click(this.elements.selects.flag)
-    I.wait(1)
-    I.click(this.elements.listElements.Moldavia)
-    I.fillFIeld(this.elements.fields.phone, "22737794")
-    I.fillField(this.elements.fields.email, "http://www.linella.md")
-    I.fillFIeld(this.elements.fields.password, "12345")
-    I.fillfield(this.elements.fields.passwordConfirm, "12345")
-    this.clickConfirmTerms()
-    this.clickSubmitButton()
-  }
+  clickConfirmTerms() {
+    I.click(this.checkboxes.confirmTerms);
+  },
+  choseLegal() {
+    I.click(this.elements.selects.userClass);
+    I.seeElement(this.elements.lists);
+    I.wait(1);
+    I.click(this.elements.listElements.userClasses.legal);
+  },
+  choseAgent() {
+    I.click(this.elements.selects.userRole);
+    I.seeElement(this.elements.lists);
+    I.wait(1);
+    I.click(this.elements.listElements.userRoles.agent);
+  },
+  choseMoldavia() {
+    I.click(this.elements.selects.country);
+    I.seeElement(this.elements.lists);
+    I.click(this.elements.listElements.Moldavia);
+  },
+  choseMoldaviaCode() {
+    I.click(this.elements.selects.flag);
+    I.wait(1);
+    I.click(this.elements.listElements.Moldavia);
+  },
+  createLegalAgentMoldova() {
+    this.choseLegal();
+    this.choseAgent();
+    this.choseMoldavia();
+    I.dontSeeElement(this.elements.fields.inn);
+    I.seeElement(this.elements.fields.nonResidentInn);
+    I.seeElement(this.elements.fields.nonResidentCompanyName);
+    I.fillFIeld(
+      this.elements.fields.nonResidentInn,
+      this.credentials.Moldavia.inn
+    );
+    I.fillfiled(
+      this.elements.fields.nonResidentCompanyName,
+      this.credentials.Moldavia.companyName
+    );
+    this.choseMoldaviaCode();
+    I.fillFIeld(this.elements.fields.phone, this.credentials.Moldavia.phone);
+    I.fillField(this.elements.fields.email, this.credentials.Moldavia.email);
+    I.fillFIeld(this.elements.fields.password, this.credentials.password);
+    I.fillfield(
+      this.elements.fields.passwordConfirm,
+      this.credentials.password
+    );
+    this.clickConfirmTerms();
+    this.clickSubmitButton();
+  },
 };
