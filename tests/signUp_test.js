51754c29d62bfb0734ce455014615d73933206e8
Aleksandr Ivanov <ivanov@finleo.ru>
2023-05-29 18:33:50 +0500

    fix: format code

--- tests/signUp_test.js
+++ tests/signUp_test.js
@@ -1,10 +1,13 @@
-const { hooks } = require("../helpers/hooks")
+const { hooks } = require("../helpers/hooks");
 
-Feature("Регистрация нового пользователя")
-Before(hooks.basePage)
+Feature("Регистрация нового пользователя");
+Before(hooks.basePage);
 
-Scenario.only("Юридическое лицо, резидент молдовы, агент", ({ I, signInPage, signUpPage}) => {
-    signInPage.goToSignUpPage()
-    signUpPage.createLegalAgentMoldova()
-    I.amOnPage("/home")
-})
+Scenario.only(
+  "Юридическое лицо, резидент Молдовы, агент",
+  ({ I, signInPage, signUpPage }) => {
+    signInPage.goToSignUpPage();
+    signUpPage.createLegalAgentMoldova();
+    I.amOnPage("/home");
+  }
+);
