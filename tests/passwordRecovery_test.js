da33f2e3c7600691fd8b29026ad68ee777b98bef
Sashka <aitmn08@gmail.com>
2023-06-29 23:01:24 +0500

    fix: rename scenario

--- tests/passwordRecovery_test.js
+++ tests/passwordRecovery_test.js
@@ -2,11 +2,11 @@ const assert = require("assert");
 const { faker } = require("@faker-js/faker");
 const { errorMessage } = require("../pages/passwordRecoveryPage");
 const { hooks } = require("../helpers/hooks");
-Feature("Восстановления пароля");
+Feature("Тестирование экрана восстановления пароля");
 
 Before(hooks.passwordRecovery);
 Scenario(
-  'Кнопка "Восстановить" задизейблена если не заполнить поле',
+  "Попытки восстановления пароля",
   async ({ I, passwordRecoveryPage }) => {
     passwordRecoveryPage.recoveryButtonDisabled(""); // Кнопка "Восстановить" задизейблена если не заполнить поле
     I.seeInCurrentUrl("/password-recovery");
@@ -18,9 +18,9 @@ Scenario(
     const notFound = await I.grabTextFrom(errorMessage);
     I.seeInCurrentUrl("/password-recovery");
     assert.equal(notFound, "Пользователь с таким адресом не найден");
-    passwordRecoveryPage.clearField()
+    passwordRecoveryPage.clearField();
     passwordRecoveryPage.recoveryPassword(process.env.ADMIN_EMAIL); // Восстановления пароля при вводе зарегистрированного email
-    I.wait(1)
+    I.wait(1);
     I.see("На Ваш email отправлено письмо со ссылкой для сброса пароля");
   }
 );
