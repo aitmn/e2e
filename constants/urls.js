a584b7ffd2407cb8ed2491ba08d0d535504be6b6
Baynov Ivan <Jhon0@yandex.ru>
2023-08-09 16:19:21 +0800

    chore: some env imporvments

--- constants/urls.js
+++ constants/urls.js
@@ -1,2 +1,4 @@
-export const SIGN_IN_URL = `${process.env.BASE_URL}/signin`;
-export const HOME_PAGE_URL = `${process.env.BASE_URL}/home`;
+module.exports = {
+	SIGN_IN_URL: `${process.env.FRONTEND_URL}/signin`,
+	HOME_PAGE_URL: `${process.env.FRONTEND_URL}/home`,
+};
